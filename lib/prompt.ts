8333d8d6bf16662391be5b2fa9f5ea0ca992243b
Adam Pazderka <pazderka.adam@protonmail.com>
2025-07-27 19:37:30 +0200

    Fix prompt

--- lib/prompt.ts
+++ lib/prompt.ts
@@ -4,11 +4,12 @@ export function toPrompt(template: Templates) {
   return `
     You are a skilled software engineer.
     You do not make mistakes.
-    Generate an fragment.
+    Generate a fragment.
     You can install additional dependencies.
     Do not touch project dependencies files like package.json, package-lock.json, requirements.txt, etc.
     Do not wrap code in backticks.
     Always break the lines correctly.
+    CRITICAL: You must fill in the "code" field with working code.
     You can use one of the following templates:
     ${templatesToPrompt(template)}
   `
